98b1b02bed9363eb63269d81eacca6e45944a2aa
Rayn Yoo <ryan-yoo@Raynui-MacBookPro.local>
2019-08-04 14:48:10 +0900

    Delayed Initialization

--- PropertyWrapper.playground/Pages/DelayedInitialization.xcplaygroundpage/Contents.swift
+++ PropertyWrapper.playground/Pages/DelayedInitialization.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 @propertyWrapper
-struct DelayedMutable<Value> {
+class DelayedMutable<Value> {
     private var _value: Value? = nil
     
     var wrappedValue: Value {
@@ -17,8 +17,10 @@ struct DelayedMutable<Value> {
             _value = newValue
         }
     }
+    var projectedValue: DelayedMutable { self }
     
-    mutating func reset() {
+    /// 이 함수를 사용하기 위해서는 DelayedMutable을 struct가 아닌 class로 선언하는 방법 밖에 없을 것 같다.
+    func reset() {
         _value = nil
     }
 }
@@ -42,5 +44,35 @@ struct DelayedImmutable<Value> {
         }
     }
 }
+
+class Foo {
+    @DelayedMutable var x: Int
+    @DelayedImmutable var y: Int
+    
+    /// x, y 모두 non-optionl로 선언했지만 initializer에서 초기화하지 않는다.
+    init() {
+    }
+    
+    func initializeX(x: Int) {
+        self.x = x
+    }
+    
+    func initializeY(y: Int) {
+        self.y = y
+    }
+}
+
+var foo = Foo()
+foo.x = 1
+foo.y = 2
+(foo.x, foo.y)
+
+foo.x = 3
+(foo.x, foo.y)
+
+foo.$x.reset()
+//foo.x // crash
+
+//foo.y = 4 // crash!
 
 //: [Next](@next)
